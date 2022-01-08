098ef08564e851392eb08fade94b134f761d2968
developer <developer@industria-i.com.br>
2022-01-08 12:21:54 +0000

    [FEATURE] Some visuals in the tickets card.

--- apps/ng-rails/src/app/dashboard/dashboard-page/material.module.ts
+++ apps/ng-rails/src/app/dashboard/dashboard-page/material.module.ts
@@ -3,12 +3,16 @@ import { MatCardModule } from '@angular/material/card';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { MatGridListModule } from '@angular/material/grid-list';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatButtonModule } from '@angular/material/button';
 
 const MATERIAL_MODULES = [
   MatCardModule,
   MatDividerModule,
   MatProgressBarModule,
-  MatGridListModule
+  MatGridListModule,
+  MatChipsModule,
+  MatButtonModule
 ];
 
 @NgModule({
