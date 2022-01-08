c57dd1b3e3b405af5b4576e9e95f2518529782bf
developer <developer@industria-i.com.br>
2022-01-08 10:38:57 +0000

    [BUG] Cors configuration to return data from different domain.

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -13,6 +13,26 @@ async function bootstrap() {
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3333;
+
+  // Ampliar o limite do body
+  // app.use(bodyParser.json({ limit: '50mb' }));
+  // app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+
+  app.enableCors();
+
+  // app.use((req, res, next) => {
+  //   res.header("Access-Control-Allow-Origin", "*");
+  //   res.header(
+  //     "Access-Control-Allow-Headers",
+  //     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  //   );
+  //   if (req.method == "OPTIONS") {
+  //     res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+  //     return res.status(200).json({});
+  //   }
+
+  //   next();
+  // });
   await app.listen(port);
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
