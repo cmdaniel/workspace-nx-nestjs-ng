96225a4de3e87c9ead18669382165a7f34120cac
developer <developer@industria-i.com.br>
2022-01-08 22:09:06 +0000

    [DEVOPS] Serverless configuration.
    Starting to configure a Serverless deployment on AWS.
    It is not finished.

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -5,6 +5,8 @@
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import { ServerlessNestjsApplicationFactory } from 'serverless-lambda-nestjs';
+import { APIGatewayProxyHandler } from 'aws-lambda';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -14,13 +16,34 @@ async function bootstrap() {
 
   const port = process.env.PORT || 3333;
 
-  app.enableCors();
+  app.enableCors({
+    origin: '*',
+    allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept',
+  });
 
-  await app.listen(port);
+  await app.listen(port, () => {
+    Logger.log('Listening at http://localhost:' + port + '/' + globalPrefix);
+  });
 
-  Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
-  );
 }
 
-bootstrap();
+/** Run Nestjs application locally. */
+if (process.env.NX_CLI_SET) {
+  bootstrap();
+}
+
+/** Run Nestjs application in AWS Lambda. */
+export const handler: APIGatewayProxyHandler = async (event, context) => {
+  const app = new ServerlessNestjsApplicationFactory<AppModule>(
+    AppModule,
+    {
+      // NestFactory.create's option object
+      cors: {
+        origin: '*',
+        allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept',
+      },
+    }
+  );
+  const result = await app.run(event, context);
+  return result;
+};
